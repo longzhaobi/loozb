cd48d7678dfedf22cdddacb42832eb26fd529015
龙召碧 <714037058@qq.com>
2017-03-20 17:22:49 +0800

    修改地址

--- loozb-common/src/main/java/com/loozb/core/filter/SimpleCORSFilter.java
+++ loozb-common/src/main/java/com/loozb/core/filter/SimpleCORSFilter.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 /**
  * 支持跨域调用
+ * 因为采用代理模式，所以此程序不再使用
  * @author LONGZB
  *
  */
@@ -17,10 +18,10 @@ public class SimpleCORSFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res,
 			FilterChain chain) throws IOException, ServletException {
 	    HttpServletResponse response = (HttpServletResponse) res;
-            response.setHeader("Access-Control-Allow-Origin", "http://csl.loozb.com");
+            response.setHeader("Access-Control-Allow-Origin", "*");
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
             response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers","x-requested-with,Access-Control-Allow-Origin,EX-SysAuthToken,EX-JSESSIONID");
+            response.setHeader("Access-Control-Allow-Headers","x-requested-with,Access-Control-Allow-Origin,Access-Token");
             response.setHeader("Access-Control-Allow-Credentials", "true");
             chain.doFilter(req, res);
 
